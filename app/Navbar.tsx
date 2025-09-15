9990fc3b7c2f102d87630890185b61dc818b14b9
billxllid <zangjiaao@yahoo.com>
2025-09-15 15:12:11 +0800

    Fafctor: Make navbar can handle multiplate item

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -10,6 +10,9 @@ import {
   SidebarGroupContent,
   SidebarMenu,
   SidebarMenuButton,
+  SidebarMenuSub,
+  SidebarMenuSubButton,
+  SidebarMenuSubItem,
 } from "@/components/ui/sidebar";
 import {
   Home,
@@ -20,6 +23,7 @@ import {
   User2,
   ChevronUp,
   LogOutIcon,
+  ChevronRight,
 } from "lucide-react";
 import {
   DropdownMenu,
@@ -27,6 +31,11 @@ import {
   DropdownMenuTrigger,
   DropdownMenuItem,
 } from "@/components/ui/dropdown-menu";
+import {
+  Collapsible,
+  CollapsibleTrigger,
+  CollapsibleContent,
+} from "@/components/ui/collapsible";
 
 const menuItems = [
   {
@@ -38,6 +47,18 @@ const menuItems = [
     title: "Follow",
     url: "/follow",
     icon: Inbox,
+    items: [
+      {
+        title: "Settings",
+        url: "/follow/settings",
+        icon: Settings,
+      },
+      {
+        title: "Content",
+        url: "/follow/content",
+        icon: Inbox,
+      },
+    ],
   },
   {
     title: "Report",
@@ -77,25 +98,48 @@ const Navbar = () => {
           <h1 className="text-xl">Oak Research</h1>
         </div>
       </SidebarHeader>
+
       <SidebarContent>
         <SidebarGroup>
           <SidebarGroupLabel>Menu</SidebarGroupLabel>
           <SidebarGroupContent>
             <SidebarMenu>
               {menuItems.map((item) => (
-                <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild>
-                    <a href={item.url}>
-                      <item.icon />
-                      <span>{item.title}</span>
-                    </a>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
+                <Collapsible key={item.title} className="group/collapsible">
+                  <SidebarMenuItem>
+                    <CollapsibleTrigger asChild>
+                      <SidebarMenuButton>
+                        <item.icon />
+                        <span>{item.title}</span>
+                        {item.items ? (
+                          <ChevronRight className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-90" />
+                        ) : null}
+                      </SidebarMenuButton>
+                    </CollapsibleTrigger>
+                    {item.items ? (
+                      <CollapsibleContent>
+                        <SidebarMenuSub>
+                          {item.items.map((item) => (
+                            <SidebarMenuSubItem key={item.title}>
+                              <SidebarMenuSubButton asChild>
+                                <a href={item.url}>
+                                  <item.icon />
+                                  <span>{item.title}</span>
+                                </a>
+                              </SidebarMenuSubButton>
+                            </SidebarMenuSubItem>
+                          ))}
+                        </SidebarMenuSub>
+                      </CollapsibleContent>
+                    ) : null}
+                  </SidebarMenuItem>
+                </Collapsible>
               ))}
             </SidebarMenu>
           </SidebarGroupContent>
         </SidebarGroup>
       </SidebarContent>
+
       <SidebarFooter>
         <SidebarMenu>
           <SidebarMenuItem>
