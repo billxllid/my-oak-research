740fe3f02f1b0220a6bebeef09cd3316d6555cbc
billxllid <zangjiaao@gmail.com>
2025-09-17 11:26:12 +0800

    优化 FollowSettingNav 组件的边距设置，并调整报告管理和模板页面的网格布局，以支持更好的响应式设计。

--- app/report/manage/page.tsx
+++ app/report/manage/page.tsx
@@ -48,7 +48,7 @@ const ReportManage = () => {
       </div>
 
       <div className="max-h-[calc(100vh-170px)] overflow-y-auto">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4">
           {reports.map((template) => (
             <ReportCard key={template.id} {...template} />
           ))}
--- app/report/template/page.tsx
+++ app/report/template/page.tsx
@@ -92,7 +92,7 @@ const ReportTemplate = () => {
       </div>
 
       <div className="max-h-[calc(100vh-170px)] overflow-y-auto">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4">
           {templates.map((template) => (
             <ReportTemplateCard key={template.id} {...template} />
           ))}
